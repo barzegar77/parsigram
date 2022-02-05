bcda6360bf98b3d908274748ab684559acaca8fa
barzegar77 <65482754+barzegar77@users.noreply.github.com>
2022-02-05 16:48:38 +0330

    end section 2

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,14 @@ namespace API
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             }
             );
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApplication1", Version = "v1" });
@@ -53,6 +61,8 @@ namespace API
             //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
